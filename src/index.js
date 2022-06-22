ac0b9ef3fbedf6e5027bbc49ed84dc0469550e40
Areliya13 <sharipovatamara@gmail.com>
2022-06-23 01:45:13 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+
+    let result = 0;
+    while (n > 0) {
+        result = result * 10 + n % 10;
+        n = (n - n % 10) / 10;
+    }
+    return result;
 }
